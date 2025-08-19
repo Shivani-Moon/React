332cf1d55e12d0adac8ddd09d67852c55be28aee
Shivani-Moon <shivani.it.2023@gmail.com>
2025-08-19 19:20:32 +0530

    01_React

---
+++ 01_basicreact/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <p>Hello World. This is my first React Project</p>
+    </>
+  )
+}
+
+export default App
---
+++ 01_basicreact/src/component/Navbar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div>
+      <p>This is Navbar</p>
+    </div>
+  )
+}
+
+export default Navbar
+
